3d0981615ed2e8848aec52477b19536b1fc2ae29
Drew Graham <drewgraham@Drews-MacBook-Pro.local>
2017-08-19 22:03:22 -0400

    created test page for displaying user data
    - pulls from an endpoint serving mocked up user data (not coming from db)
    - need to touch up CSS, it's just ripped from some w3schools bootstrap demo
    - set it as index.html for now

--- server.js
+++ server.js
@@ -7,38 +7,11 @@ var app = express();
 
 app.use(express.static("."));
 app.listen(8080, function() {
-  console.log('server running');
+  console.log('server running...');
 });
 
 app.get('/getUser', function(req, res) {
   var userJson = file.readFileSync('./data/sampleUser.json', 'utf8');
-  res.send(JSON.parse(userJson));
+  res.send(userJson);
 
-  /*
-  var seed = parseInt(req.query.seed);
-  if(seed < 0) {
-    res.send("Invalid seed (must be a positive value)");
-  }
-  else {
-    calc.once('fact', function(msg) {
-      res.send(msg);
-    });
-    calc.fact(seed);
-  }
-  */
 });
-
-/*
-app.get('/computeSum', function(req, res) {
-  var seed = parseInt(req.query.seed);
-  if(seed < 0) {
-    res.send("Invalid seed (must be a positive value)");
-  }
-  else {
-    calc.once('sum', function(msg) {
-      res.send(msg);
-    });
-    calc.sum(seed);
-  }
-});
-*/
